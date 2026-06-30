a98e712f221565cff8ef6a26bc8544d3c2540396
Ayush Dubey <ayush21dubey2005@gmail.com>
2026-06-30 21:50:22 +0530

    functions/methods basics

---
+++ Basics/Methods.java
@@ -0,0 +1,49 @@
+import java.util.*;
+public class Methods {
+    public static void main(String[] args) {
+
+        int sum = sum();//int function
+        System.out.println(sum);
+
+        String greeting = greet(); //string function 
+        System.out.println(greeting);
+
+        int ans = sum2(20,30);// pass by value 
+        System.out.println("sum of a and b is :"+ans);
+    }
+
+    static int sum(){
+        Scanner in = new Scanner (System.in);
+        System.out.println("Enter the two numbers");
+        int n1,n2;
+        n1=in.nextInt();
+        n2=in.nextInt();
+        int s= n1+n2;
+        return s; // this is the end point of the function 
+
+        String msg = myGreet("Ayush Dubey")
+    }
+    /*
+    Access modifiers| return type|name (arguments){
+    body//
+    return statement;
+    } */
+    //  Return type : It determines the valuye type which is returned by the function after the execution 
+    static String greet(){
+        String greeting = "Hii !! how are you";
+        return greeting;
+    }
+    static String mygGreet(String name){
+        String msg = "Hii"+ name + "!! how are you";
+        return msg;
+    }
+    
+    //pass by values in method calling
+
+    static int sum2(int a, int b){
+        int sum = a+b;
+        return sum;
+    }
+
+}
+
