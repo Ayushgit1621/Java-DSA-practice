ac5a18951ab917fef759b50df9de32daea2cec5d
Ayush Dubey <ayush21dubey2005@gmail.com>
2026-08-14 23:20:00 +0530

    Added question Leetcode 8 and 121

---
+++ Leetcode/Q121.java
@@ -0,0 +1,16 @@
+public class Q121 {
+    public int maxProfit(int[] prices) {
+
+        int minPrice=prices[0];
+        int maxProfit=0;
+
+        for(int i=1;i<prices.length;i++){
+            
+            int profitToday= prices[i]-minPrice;
+            maxProfit =Math.max(maxProfit,profitToday);
+            minPrice = Math.min(minPrice,prices[i]);
+        }
+        return maxProfit;
+    }
+    
+}
