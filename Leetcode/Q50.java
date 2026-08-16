b88884e516d1af9ebb49539310dddcd2263171ad
Ayush Dubey <ayush21dubey2005@gmail.com>
2026-08-16 23:16:07 +0530

    Solved two recursion questions of Q509 and Q50 on Leetcode

---
+++ Leetcode/Q50.java
@@ -0,0 +1,23 @@
+package Leetcode;
+
+class Q50 {
+    public double myPow(double x, int n) {
+        long N =n;
+        if(N<0){
+            return 1.0/recPow(x,-N);
+        }
+        return recPow(x,N);
+    }
+    double recPow(double x, long n){
+        if(n==0){
+            return 1.0;
+        }
+        double brk = recPow(x,n/2);
+        if(n%2==0){
+            return brk*brk;
+        }
+        else{
+            return brk*brk*x;
+        }
+    }
+}
