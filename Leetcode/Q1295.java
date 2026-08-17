6ba886bb90ef4aac9b3446591c3f2ba6f8f7b76f
Ayush Dubey <ayush21dubey2005@gmail.com>
2026-08-17 23:36:28 +0530

    Solved ques 1295 and 1431 on leetcode

---
+++ Leetcode/Q1295.java
@@ -0,0 +1,19 @@
+package Leetcode;
+
+class Q1295 {
+    public int findNumbers(int[] nums) {
+        int n= nums.length;
+        int res=0;
+        for(int i=0;i<n;i++){
+            int count =0;
+             while(nums[i]>0){
+                nums[i]/=10;
+                count++;
+            }
+            if(count%2==0){
+            res++;
+            }                     
+        }
+        return res; 
+    }
+}
---
+++ Leetcode/Q1431.java
@@ -0,0 +1,25 @@
+package Leetcode;
+
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int n= candies.length; int max=0;
+        List<Boolean> res= new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(candies[i]>max){
+                max= candies[i];
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(candies[i]+extraCandies >= max){
+                res.add(true);
+            }
+            else{
+                res.add(false);
+            }
+        }
+        return res;
+    }
+}
